64c1a2c8f3d44193aa79ee73c5be33cf251f4f88
akash0h <masumaktar2409@gmail.com>
2025-02-18 21:39:16 +0530

    Create If_Else_Leap_Year.c

---
+++ If_Else_Leap_Year.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() 
+{
+    int year;
+    scanf("%d",&year);
+    if(year>1 && year%4==0)
+    {
+        printf("Yes, %d is a leap year.",year);
+    }
+    else if(year<1)
+    {
+        printf("The given year %d is Invalid year.",year);
+    }
+    else if(year%100==0 && year%400==0)
+        {
+            printf("Yes, %d is a leap year.",year);
+        }
+    
+    else if(!(year%4==0))
+    {
+        printf("No, %d is not a leap year.",year);
+    }
+    else if(year>100000)
+    {
+        printf("The given year %d is Invalid year.",year);
+    }
+    
+    
+    return 0;
+}
