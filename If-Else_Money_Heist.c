8f3212aedd6b9e351416935251bd3fbec2f5189d
akash0h <masumaktar2409@gmail.com>
2025-02-22 23:42:35 +0530

    Create If-Else_Money_Heist.c
    If-Else_Money_Heist

---
+++ If-Else_Money_Heist.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+int main() {
+
+    int a,b,c,d,e,f,g,h,i,j, pos1,pos2, pos3, rot1,rot2,rot3;
+    scanf("%d %d %d %d %d %d %d %d %d %d\n",&a,&b,&c,&d,&e,&f,&g,&h,&i,&j);
+    scanf("%d %d\n", &pos1, &rot1);
+    scanf("%d %d\n", &pos2, &rot2);
+    scanf("%d %d\n", &pos3, &rot3);
+    if(pos1==0 || pos2==0 || pos3==0) {
+        if(pos1==0) {
+            a=rot1+a;
+        }
+        else if(pos2==0) {
+            a=rot2+a;
+        }
+        else if(pos3==0) {
+            a=rot3+a;
+        }
+        if(a>9) {
+            a=a-10;
+        }
+    }
+    if(pos1==1 || pos2==1 || pos3==1) {
+        if(pos1==1) {
+            b=rot1+b;
+        }
+        else if(pos2==1) {
+            b=rot2+b;
+        }
+        else if(pos3==1) {
+            b=rot3+b;
+        }
+        if(b>9) {
+            b=b-10;
+        }
+    }
+    if(pos1==2 || pos2==2 || pos3==2) {
+        if(pos1==2) {
+            c=rot1+c;
+        }
+        else if(pos2==2) {
+            c=rot2+c;
+        }
+        else if(pos3==2) {
+            c=rot3+c;
+        }
+        if(c>9) {
+            c=c-10;
+        }
+    }
+    if(pos1==3 || pos2==3 || pos3==3) {
+        if(pos1==3) {
+            d=rot1+d;
+        }
+        else if(pos2==3) {
+            d=rot2+d;
+        }
+        else if(pos3==3) {
+            d=rot3+d;
+        }
+        if(d>9) {
+            d=d-10;
+        }
+    }
+    if(pos1==4 || pos2==4 || pos3==4) {
+        if(pos1==4) {
+            e=rot1+e;
+        }
+        else if(pos2==4) {
+            e=rot2+e;
+        }
+        else if(pos3==4) {
+            e=rot3+e;
+        }
+        if(e>9) {
+            e=e-10;
+        }
+    }
+    if(pos1==5 || pos2==5 || pos3==5) {
+        if(pos1==5) {
+            f=rot1+f;
+        }
+        else if(pos2==5) {
+            f=rot2+f;
+        }
+        else if(pos3==5) {
+            f=rot3+f;
+        }
+        if(f>9) {
+            f=f-10;
+        }
+    }
+    if(pos1==6 || pos2==6 || pos3==6) {
+        if(pos1==6) {
+            g=rot1+g;
+        }
+        else if(pos2==6) {
+            g=rot2+g;
+        }
+        else if(pos3==6) {
+            g=rot3+g;
+        }
+        if(g>9) {
+            g=g-10;
+        }
+    }
+    if(pos1==7 || pos2==7 || pos3==7) {
+        if(pos1==7) {
+            h=rot1+h;
+        }
+        else if(pos2==7) {
+            h=rot2+h;
+        }
+        else if(pos3==7) {
+            h=rot3+h;
+        }
+        if(h>9) {
+            h=h-10;
+        }
+    }
+    if(pos1==8 || pos2==8 || pos3==8) {
+        if(pos1==8) {
+            i=rot1+i;
+        }
+        else if(pos2==8) {
+            i=rot2+i;
+        }
+        else if(pos3==8) {
+            i=rot3+i;
+        }
+        if(i>9) {
+            i=i-10;
+        }
+    }
+    if(pos1==9 || pos2==9 || pos3==9) {
+        if(pos1==9) {
+            j=rot1+j;
+        }
+        else if(pos2==9) {
+            j=rot2+j;
+        }
+        else if(pos3==9) {
+            j=rot3+j;
+        }
+        if(j>9) {
+            j=j-10;
+        }
+    }
+    printf("%d %d %d %d %d %d %d %d %d %d",a,b,c,d,e,f,g,h,i,j);
+
+    return 0;
+}
