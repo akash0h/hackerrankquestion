703236c2068868831f73a881f0d4455c60c16b7a
akash0h <masumaktar2409@gmail.com>
2025-02-23 23:26:32 +0530

    Create Bitwise_Even or Odd.c

---
+++ Bitwise_Even or Odd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    a = a & 1;
+    if(a == 0){
+        printf("Even");
+    }
+    else
+    {
+        printf("Odd");
+    }
+    return 0;
+}
