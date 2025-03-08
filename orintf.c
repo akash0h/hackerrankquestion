2a08fdc3d73b65f4879619fa52812583c01b14ed
akash0h <masumaktar2409@gmail.com>
2025-03-08 22:32:45 +0530

    Create orintf.c

---
+++ orintf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    scanf("%d",&num);
+    if(num==0)
+    {
+        printf("No");
+    }
+    else if((num&(num-1))==0){
+        printf("Yes");
+    }
+        else{
+            printf("No");
+        }
+return 0;
+}
