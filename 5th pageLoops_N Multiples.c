303be6e40fc07099d06e62c325114743c7f93c90
akash0h <masumaktar2409@gmail.com>
2025-03-27 23:41:56 +0530

    Create 5th pageLoops_N Multiples.c

---
+++ 5th pageLoops_N Multiples.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n,x;
+    
+    scanf("%d %d",&n, &x);
+    
+        for(int i=1; i<=n; i++){
+            int a=x*i;
+            printf("%d ",a);
+        }
+            
+    
+    return 0;
+}
