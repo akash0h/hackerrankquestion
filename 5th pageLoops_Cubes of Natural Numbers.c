53cbb07272e741ad6775f65b21b965c4b4d6ec27
akash0h <masumaktar2409@gmail.com>
2025-03-27 23:43:01 +0530

    Create 5th pageLoops_Cubes of Natural Numbers.c

---
+++ 5th pageLoops_Cubes of Natural Numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    scanf("%d",&num);
+
+    for(int i=1; i<=num; ++i)
+    {
+        int c=(i*i)*i;
+        printf("%d ",c);
+    }
+    return 0;
+}
