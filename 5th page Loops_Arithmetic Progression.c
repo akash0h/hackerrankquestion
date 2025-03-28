7b10d3e7a53a5dcbba194dd82fd107457d0cc4b3
akash0h <masumaktar2409@gmail.com>
2025-03-28 23:55:15 +0530

    Create 5th page Loops_Arithmetic Progression.c

---
+++ 5th page Loops_Arithmetic Progression.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n,a,d;
+    scanf("%d\n",&n);
+    scanf("%d %d",&a,&d);
+    for(int i=1; i<=n; ++i)
+    {
+        
+        printf("%d ",a);
+        a=a+d;
+    }
+    return 0;
+}
