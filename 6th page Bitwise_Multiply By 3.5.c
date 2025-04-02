603194d4b7b876bad7972a2fdfe15d1ce7745088
akash0h <masumaktar2409@gmail.com>
2025-04-02 23:12:20 +0530

    Create 6th page Bitwise_Multiply By 3.5.c

---
+++ 6th page Bitwise_Multiply By 3.5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num,b,c;
+    scanf("%d",&num);
+    b=num>>1;
+    c=((num<<2) - b);
+    if(num%2==0){
+       printf("%d",c); 
+    }
+    else if(num%2 !=0){
+        printf("%d",c-1);
+    }
+    
+    
+    return 0;
+}
