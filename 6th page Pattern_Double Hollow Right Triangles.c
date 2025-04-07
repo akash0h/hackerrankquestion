0a2788c00b5fb462790eb72d27233eba6a00a28c
akash0h <masumaktar2409@gmail.com>
2025-04-07 23:36:04 +0530

    Create 6th page Pattern_Double Hollow Right Triangles.c

---
+++ 6th page Pattern_Double Hollow Right Triangles.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    if(n>1)
+    {
+        for(int i=1; i<=n; ++i)
+    {
+        for(int j=1; j<=i; ++j)
+        {
+            if(j==1 || i==n || j==2*n || j==i)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        for(int k=1; k<=2*n-(2*i); ++k)
+        {
+            printf(" ");
+        }
+        for(int j=1; j<=i; ++j)
+        {
+            if(j==1 || i==n || j==2*n || j==i)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    }
+    else
+    {
+        printf("Shape Not Possible");
+    }
+    
+    return 0;
+}
