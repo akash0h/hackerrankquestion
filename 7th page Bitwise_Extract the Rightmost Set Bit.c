142b22c8549670a20b034a8843fffc3751461f7d
akash0h <masumaktar2409@gmail.com>
2025-04-10 21:49:19 +0530

    Create 7th page Bitwise_Extract the Rightmost Set Bit.c

---
+++ 7th page Bitwise_Extract the Rightmost Set Bit.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    if(n==0)
+    {
+        printf("0");
+    }
+    else
+    {
+        int i;
+        for(i=1;(n&1)==0;++i)
+        {
+            n=n>>1;
+        }
+        printf("%d",i);
+    }
+
+        
+        
+        
+    
+    return 0;
+}
