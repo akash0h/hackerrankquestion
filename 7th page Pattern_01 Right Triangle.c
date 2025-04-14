25509a1d0451cab07b2fe5823007f6fee028b029
akash0h <masumaktar2409@gmail.com>
2025-04-14 22:46:42 +0530

    Create 7th page Pattern_01 Right Triangle.c

---
+++ 7th page Pattern_01 Right Triangle.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    if(n>0 && n<=50)
+    {
+        for(int i=1; i<=n; ++i)
+        {
+            for(int j=1; j<=i; ++j)
+            {
+                if(i%2==0 && j%2!=0)
+                {
+                    printf("0");
+                }
+                else if(i%2!=0 && j%2==0)
+                {
+                    printf("0");
+                }
+                else
+                {
+                    printf("1");
+                }
+            }
+            printf("\n");
+        }
+    }
+    else
+    {
+        printf("Shape Not Possible");
+    }
+    return 0;
+}
