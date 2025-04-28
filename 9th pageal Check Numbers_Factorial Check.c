feabb231e9f7678e4231365cc6f7e98c9a5814d7
akash0h <masumaktar2409@gmail.com>
2025-04-28 14:22:07 +0530

    Create 9th pageal Check Numbers_Factorial Check.c

---
+++ 9th pageal Check Numbers_Factorial Check.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    long int n;
+    scanf("%ld",&n);
+    long int i=1;
+    while(n>0)
+    {
+        
+        if(n%i!=0)
+        {
+            break;
+        }
+       
+        else if(n%i==0)
+        {
+         n=n/i;
+        }
+        ++i;
+        
+    }
+    if(n==1)
+    {
+        printf("%ld",i-1);
+    }
+    else{
+        printf("NO");
+    }
+    
+    return 0;
+}
