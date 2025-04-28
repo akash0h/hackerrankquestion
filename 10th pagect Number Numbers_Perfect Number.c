b79d28e4bc8ff002419b5df865ef21c16754cbad
akash0h <masumaktar2409@gmail.com>
2025-04-28 14:24:27 +0530

    Create 10th pagect Number Numbers_Perfect Number.c

---
+++ 10th pagect Number Numbers_Perfect Number.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int i;
+    int sum=0;
+    for(i=1; i<=n-1;++i)
+    {
+        if(n%i==0)
+        {
+            
+            sum=sum + i;
+            
+        }
+        
+    }
+    if(sum==n)
+    {
+        printf("YES");
+        
+    }
+    else{
+        printf("NO");
+    }
+    return 0;
+}
