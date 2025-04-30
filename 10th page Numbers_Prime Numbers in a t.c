ef5b00dc474a62e342d1220cd54e3d7706ab07b8
akash0h <masumaktar2409@gmail.com>
2025-04-30 23:36:34 +0530

    Create 10th page Numbers_Prime Numbers in a t.c

---
+++ 10th page Numbers_Prime Numbers in a t.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    long long n;
+    scanf("%lld",&n);
+    long long  num=0;
+    long long  count=0;
+    if(n<0)
+    {
+        n=-n;
+    }
+    while(n!=0)
+    {
+        num=n%10;
+
+        if(num==2 || num==3 || num==5 || num==7)
+        {
+            ++count;
+
+        }
+
+        n=n/10;
+    }
+    printf("%lld",count);
+    
+}
